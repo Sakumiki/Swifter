5dd14a071288d292fdebcc1c19c8633886e453b2
Saku <itowdjg3104@gmail.com>
2016-11-24 12:00:45 +0900

    aa
    aa

---
+++ school/system/add.php
@@ -0,0 +1,45 @@
+﻿<html>
+<head>
+<title>test201.php</title>
+</head>
+<body>
+
+<?php
+
+//general
+function fncIn($ban,$ge,$bl,$bir,$se){
+
+	$result = pg_query("insert into tbl_data values('" .$ban. "','" .$ge. "','" .$bl. "','" .$bir. "','" .$se."');");
+	if($result)	print("okあああ");
+	else		print("no");
+}
+
+function fncUp($ban,$ge,$bl,$bir,$se){
+
+	$result = pg_query("update tbl_data set gender = '" .$ge. "', blood ='" .$bl. "', birth ='" .$bir. "', seiza='" .$se."' where bango ='". $ban ."';");
+	if($result)	print("okいい");
+	else		print("no");
+}
+
+
+//quiestion
+
+function fncInq($ban,$one,$two,$thr,$four,$fiv){
+
+	$result = pg_query("insert into tbl_data values('".$ban."','" .$one. "','" .$two. "','" .$thr. "','" .$four. "','" .$fiv."');");
+	if($result)	print("okあああ");
+	else		print("no");
+}
+
+function fncUpq($ban,$one,$two,$thr,$four,$fiv){
+
+	$result = pg_query("update tbl_data set one = '" .$ge. "', two ='" .$bl. "', three ='" .$bir. "', four='" .$se."', five='".$fiv."' where bango ='". $ban ."';");
+	if($result)	print("okいい");
+	else		print("no");
+}
+
+?>
+
+</body>
+
+</html>
---
+++ school/system/login.php
@@ -0,0 +1,53 @@
+﻿<?php
+
+require_once __DIR__ . '/func.php';
+require_unlogined_session();
+
+$conn = pg_connect("dbname=db_3836 user=d33836 host=192.168.109.210");
+
+// ユーザから受け取ったユーザ名
+$ban = filter_input(INPUT_POST, 'bango');
+if($ban < 10) $bana = "0".$ban;
+$bango = "d338".$bana;
+print($bango);
+$result = pg_query("select * from tbl_namae where bango ='".$bango."';");
+
+// POSTメソッドのときのみ実行
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    if ($result) {
+        // 認証が成功したとき
+        // セッションIDの追跡を防ぐ
+        session_regenerate_id(true);
+         // ユーザ名をセット
+        $_SESSION['username'] = $username;
+		$_SESSION['id'] = $bango;
+        // ログイン完了後に / に遷移
+		$result = pg_query("select * from tbl_data where bango ='".$bango."'"); 
+		if(pg_numRows($result) != 0)			header('Location: home.php');
+        else if(pg_numRows($result) ==0)	header('Location: general.php');
+        exit;
+    }
+    // 認証が失敗したとき
+    // 「403 Forbidden」
+//    http_response_code(403);
+}
+
+header('Content-Type: text/html; charset=UTF-8');
+
+?>
+<!DOCTYPE html>
+<title>ログインページ[?W</title>
+<body>
+<h1>>ログインしてください</h1>
+<form method="post" action="">
+    ユーザ名: <input type="text" name="bango" value="">
+    <input type="hidden" name="token" value="<?=h(generate_token())?>">
+    <input type="submit" value="ログイン">
+</form>
+<!--
+<?php  if (http_response_code() === 403): ?>
+<p style="color: red;">ユーザ名またはパスワードが違います</p>
+<?php endif; ?>
+--></body>
+</html>
